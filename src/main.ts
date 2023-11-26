ef6c6eed1a40773a7656aa9670f5fa3524632c18
EmmanunelStocco <emmanuelrolimstocco@gmail.com>
2023-11-25 21:21:39 -0300

    chore: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/Swagger'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.setGlobalPrefix('/api')
   app.useGlobalPipes(new ValidationPipe());
+
+  
+  const config = new DocumentBuilder()
+    .setTitle('Documentação - Desafio Backend Challenge')
+    .setDescription(
+      'Aqui você consegue visualizar e testar diretamente o funcionamento de cada rota da Rest API.',
+    )
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('document', app, document);
+
+
   await app.listen(3000); //docker build
 }
 bootstrap();
