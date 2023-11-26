5ea7ae6e8de3751663757e527e5b5a1cf703b98c
EmmanunelStocco <emmanuelrolimstocco@gmail.com>
2023-11-26 12:32:30 -0300

    feat: movie CRUD base

---
+++ src/controllers/movie.controller.ts
@@ -0,0 +1,70 @@
+import {
+  Body,
+  Controller,
+  NotFoundException,
+  Param,
+  Post,
+  Get,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { MovieModel } from 'src/models/movie.model';
+import { MovieSchema } from 'src/schemas/movie.schemas';
+
+@Controller('/movie')
+export class UserController {
+  constructor(
+    @InjectRepository(MovieModel) private model: Repository<MovieModel>,
+  ) {}
+
+  @Post()
+  public async create(
+    @Body() body: MovieSchema,
+  ): Promise<{ data: MovieModel }> {
+    const movieCreated = await this.model.save(body);
+    return { data: movieCreated };
+  }
+
+  @Get(':id')
+  public async getOne(@Param('id') id: number): Promise<{ data: MovieModel }> {
+    const movie = await this.model.findOne({ where: { id } });
+    if (!movie) {
+      throw new NotFoundException(`No movie with this id was found!`);
+    }
+    return { data: movie };
+  }
+
+  @Get()
+  public async getAll(): Promise<{ data: MovieModel[] }> {
+    const movieList = await this.model.find();
+    return { data: movieList };
+  }
+
+  @Put(':id')
+  public async update(
+    @Param('id') id: number,
+    @Body() body: MovieSchema,
+  ): Promise<{ data: MovieModel }> {
+    const movie = await this.model.findOne({ where: { id } });
+
+    if (!movie) {
+      throw new NotFoundException(`No movie with this id was found.`);
+    }
+
+    await this.model.update(id, body);
+
+    return { data: await this.model.findOne({ where: { id } }) };
+  }
+
+  @Delete(':id')
+  public async delete(@Param('id') id: number): Promise<{ data: string }> {
+    const movie = await this.model.findOne({ where: { id } });
+    if (!movie) {
+      throw new NotFoundException(`No movie with this id was found.`);
+    }
+    await this.model.delete(id);
+    return { data: `Movie id:${id} has been successfully deleted!` };
+  }
+}
