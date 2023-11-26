0f6fcdefcc655046c7096a2da09702e3ab1814e1
EmmanunelStocco <emmanuelrolimstocco@gmail.com>
2023-11-26 11:59:07 -0300

    feat: Movie Model created

---
+++ src/models/movie.model.ts
@@ -0,0 +1,34 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class Movie {
+  @PrimaryGeneratedColumn()
+  id: number;
+  
+  @Column()
+  title: string;
+
+  @Column({ type: 'int', nullable: true })
+  year: number;
+
+  @Column({ type: 'text', nullable: true })
+  synopsis: string;
+
+  @Column({ type: 'varchar', length: 100, nullable: true })
+  director: string;
+
+  @Column({ type: 'varchar', length: 100, nullable: true })
+  nationality: string;
+
+  @Column({ type: 'varchar', length: 100, nullable: true })
+  posterURL: string;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  @Column({ type: 'json', nullable: true })
+  genres: string[];
+}
